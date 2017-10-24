1f8a96df2f402913b20628ed2bc5632766c4c051
yehootmail <liamtoohey@gmail.com>
2017-10-24 18:21:43 -0400

    Added testing capable time counting code

--- src/main.js
+++ src/main.js
@@ -243,6 +243,8 @@ function genReminders(reminderType) {
 function loopReminders(reminderType) {
     let reminderLagMinutes = Math.floor(Math.random() * (80 - 40 + 1) + 40);
     let reminderLagMs = 1000 * 60 * reminderLagMinutes;
+    //let reminderLagMinutes = Math.floor(Math.random() * (20 - 10 + 1) + 10);
+    //let reminderLagMs = 1000 * reminderLagMinutes;
     remindersTimeout = setTimeout(function () {
         genReminders(reminderType)
         loopReminders(reminderType);
